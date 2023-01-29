70a74167d3ba647531002d0437e19b89eed2bfc3
ShcherbinaDmitry <shcherbinadmitry@gmail.com>
2023-01-29 13:19:17 +0300

    basic template setup

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -7,3 +7,14 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/urls')
+def get_urls():
+    sites = [{
+        'id': 1,
+        'name': 'Test site',
+        'last_check': '2023-01-29',
+        'response_code': 200
+    }]
+    return render_template('urls/index.html', sites=sites)
