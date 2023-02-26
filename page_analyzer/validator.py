5fa55da94626c17827d450ecbb734c2a89369059
ShcherbinaDmitry <shcherbinadmitry@gmail.com>
2023-02-26 21:19:04 +0300

    validator functions

---
+++ page_analyzer/validator.py
@@ -0,0 +1,17 @@
+from urllib.parse import urlparse
+from validators.url import url as url_validator
+
+def validate(url):
+    errors = []
+    if len(url) > 255:
+        errors.append('URL превышает 255 символов')
+    if not url_validator(url):
+        errors.append('Некорректный URL')
+    if not url:
+        errors.append('URL обязателен')
+    return errors
+
+
+def normalize(url):
+  parsed = urlparse(url)
+  return f'{parsed.scheme}://{parsed.netloc}'
